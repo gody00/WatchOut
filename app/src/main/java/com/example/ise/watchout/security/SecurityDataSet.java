738b18f14e1217fb54bf55549539e0a84347de94
gody00 <yhko905@naver.com>
2019-11-10 01:26:29 +0900

    WatchOut application_YOOHYUN

---
+++ app/src/main/java/com/example/ise/watchout/security/SecurityDataSet.java
@@ -0,0 +1,12 @@
+package com.example.ise.watchout.security;
+
+/**
+ * Created by user on 2017-08-13.
+ */
+
+public final class SecurityDataSet {
+
+    public static final String ServerUrl = "http://13.124.74.113:3000";
+
+}
+
